1b7ab5e5e3b0632bc67a3bbc15e7fa9386cae83f
IB-Projects <isaiahbuchberger@gmail.com>
2019-05-07 17:48:01 -0500

    really hope this works

---
+++ precache-manifest.36125facef0f3307fb2604e2da11da98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6d4510dd50b6c79b604",
+    "url": "/static/css/main.d782d017.chunk.css"
+  },
+  {
+    "revision": "b6d4510dd50b6c79b604",
+    "url": "/static/js/main.5479a76c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de1452e81efccccd6e66",
+    "url": "/static/css/2.e587000b.chunk.css"
+  },
+  {
+    "revision": "de1452e81efccccd6e66",
+    "url": "/static/js/2.8259ab5c.chunk.js"
+  },
+  {
+    "revision": "5d3a75c58ec2760bf6701b0ff6e38226",
+    "url": "/static/media/IB-ProjectsLogo.5d3a75c5.jpg"
+  },
+  {
+    "revision": "1c46b4421d177ecfe8b987c0f91b9032",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
